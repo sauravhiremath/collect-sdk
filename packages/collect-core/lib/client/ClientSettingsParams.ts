4d87e51fb986d3e6c2a9924a35f483881d1535d1
Saurav M. Hiremath <sauravhiremath@gmail.com>
2021-02-07 17:30:30 +0530

    feat(core): add ClientSettingsParams

---
+++ packages/collect-core/lib/client/ClientSettingsParams.ts
@@ -0,0 +1,30 @@
+import { EnvironmentName } from "../utils";
+
+export interface ClientSettingsParams {
+  /**
+   * An asynchronous callback used to return a `Promise` with the access token for requests.
+   *
+   * @example
+   * ```typescript
+   *  import { UserAuth } from "@collect/auth";
+   *
+   *  const userAuth = new UserAuth(/** parameters **);
+   *  const getToken = userAuth.getToken();
+   * ```
+   *
+   * @return The `Promise` with the access token for requests.
+   */
+  getToken: () => Promise<string>;
+
+  /**
+   * An environment that should be used to get the URL of the API Lookup Service.
+   * You can also specify a URL of your custom service.
+   *
+   * @example
+   *
+   * ```
+   *  'here' | 'here-dev' | 'http://127.0.0.1/my-local-api-service'
+   * ```
+   */
+  environment: EnvironmentName;
+}
